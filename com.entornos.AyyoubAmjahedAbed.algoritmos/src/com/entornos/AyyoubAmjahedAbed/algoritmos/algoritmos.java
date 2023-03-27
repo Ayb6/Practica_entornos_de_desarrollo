d23a694a9efca053544246b287b0e29661e3b281
Ayyoub Amjahed <ayyub2000@gmail.com>
2023-03-27 10:54:31 +0200

    Tercer commit, se actualiza el archivo algoritmos.java incluyendo ahora el metodo factorial

--- com.entornos.AyyoubAmjahedAbed.algoritmos/src/com/entornos/AyyoubAmjahedAbed/algoritmos/algoritmos.java
+++ com.entornos.AyyoubAmjahedAbed.algoritmos/src/com/entornos/AyyoubAmjahedAbed/algoritmos/algoritmos.java
@@ -1,4 +1,5 @@
 package com.entornos.AyyoubAmjahedAbed.algoritmos;
+
 /**
  * 
  * @author ayyub
@@ -7,11 +8,11 @@ package com.entornos.AyyoubAmjahedAbed.algoritmos;
 public abstract class algoritmos {
 	/**
 	 * 
-	 * 
+	 * Creo el metodo fibonacci
 	 * @param numero
 	 * @return
 	 */
-	public static int fibonacci (int numero) {
+	public static int fibonacci(int numero) {
 		int siguiente = 1, actual = 0, temporal = 0;
 		for (int x = 1; x <= numero; x++) {
 			temporal = actual;
@@ -19,5 +20,24 @@ public abstract class algoritmos {
 			siguiente = siguiente + temporal;
 		}
 		return actual;
+	}
+	
+	/**
+	 * 
+	 * Creo el metodo factorial 
+	 * @param numero
+	 * @return
+	 */
+	public static int factorial(int numero) {
+		if (numero < 0)
+			numero = numero * -1;
+		if (numero <= 0)
+			return 1;
+		int factorial = 1;
+		while (numero > 1) {
+			factorial = factorial * numero;
+			numero--;
+		}
+		return factorial;
 	}
 }
